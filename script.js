50060c3ebfffa0ff8f7578647f3236a96a3bb551
Simon Milord <simon.milord@gmail.com>
2022-04-29 19:25:39 -0400

    added search functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+let query = document.getElementById('search');
+let form = document.getElementById('form');
+
+form.onsubmit = function(event) {
+  event.preventDefault();
+  let url = "https://www.google.com/search?q=" + query.value;
+  window.open(url, "_self");
+}
